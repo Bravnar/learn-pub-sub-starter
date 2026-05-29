c71fd41d5ea8755e7140eba371dac2aae9bbfac9
Bravnar <stan.muravyev@bluewin.ch>
2026-05-29 14:15:50 +0200

    setup rabbitmq and rabbitmq-stomp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connectionString := "amqp://guest:guest@localhost:5672"
+	amqpConnection, err := amqp.Dial(connectionString)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer amqpConnection.Close()
+
+	fmt.Println("Amqp Connection Successful!")
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("\nSIGINT received, shutting down...")
 }
