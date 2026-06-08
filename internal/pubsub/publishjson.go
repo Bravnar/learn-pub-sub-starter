a8313c0d498f31180bd6306a95eea70f130e2ffb
Bravnar <stan.muravyev@bluewin.ch>
2026-06-08 19:33:02 +0200

    created message to dead queue

--- internal/pubsub/publishjson.go
+++ internal/pubsub/publishjson.go
@@ -41,7 +41,9 @@ func DeclareAndBind(
 		isAutoDelete,
 		isExclusive,
 		false,
-		nil,
+		amqp.Table{
+			"x-dead-letter-exchange": "peril_dlx",
+		},
 	)
 	if err != nil {
 		return &amqp.Channel{}, amqp.Queue{}, err
